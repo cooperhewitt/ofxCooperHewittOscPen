ad2df23dd725ca1d4cca3ec8f92e2eeda36fc677
Micah Walter <walterm@si.edu>
2016-11-18 12:31:32 -0500

    all it does is log messages

--- src/ofxCooperHewittOscPen.cpp
+++ src/ofxCooperHewittOscPen.cpp
@@ -1,12 +1,26 @@
 #include "ofMain.h"
 #include "ofxCooperHewittOscPen.h"
 
-ofxCooperHewittOscPen::ofxCooperHewittOscPen() {
-	oscReceiver.setup(8000);
+const int NUM_TAG_ARGS = 7;
+const int NUM_PEN_ARGS = 4;
+const int NUM_PEN_STATUS_ARGS = 4;
+const string TAG_OSC_ADDRESS = "/tag";
+const string PEN_OSC_ADDRESS = "/pen";
+const string PEN_DONE_OSC_ADDRESS = "/pen-done";
+const string PEN_CANCEL_OSC_ADDRESS = "/pen-cancel";
+const string READER_OSC_ADDRESS = "/reader";
+const string ALL_READERS_ADDRESS = "/readers";
+
+ofxCooperHewittOscPen::ofxCooperHewittOscPen() {	
 }
 
 ofxCooperHewittOscPen::~ofxCooperHewittOscPen() {
 
+}
+
+void ofxCooperHewittOscPen::setup(int port)
+{
+	oscReceiver.setup(port);
 }
 
 string ofxCooperHewittOscPen::getPenMessages()
@@ -14,11 +28,59 @@ string ofxCooperHewittOscPen::getPenMessages()
 	while (oscReceiver.hasWaitingMessages()) {
 		ofxOscMessage m;
 		oscReceiver.getNextMessage(&m);
-		string msg = "";
-		for (int i = 0; i < m.getNumArgs(); i++) {
-			msg += m.getArgAsString(i);
-			msg += " ";
-			ofLog() << msg << endl;
+
+		ofLogNotice("ofxCooperHewittOscPen") << m.getAddress();
+
+		if ((m.getAddress() == TAG_OSC_ADDRESS) && (m.getNumArgs() == NUM_TAG_ARGS)) {
+			tagEvent tag = {
+				m.getArgAsString(0),
+				m.getArgAsString(1),
+				m.getArgAsString(2),
+				m.getArgAsString(3),
+				m.getArgAsString(4),
+				m.getArgAsString(5),
+				m.getArgAsFloat(6)
+			};
+			ofLogNotice("ofxCooperHewittOscPen") << "Reading Tag: " << tag.tagID << " for Visit: " << tag.visitID << endl;
+		}
+		
+		else if ((m.getAddress() == PEN_OSC_ADDRESS) && (m.getNumArgs() == NUM_PEN_ARGS)) {
+			penEvent pen = {
+				m.getArgAsString(0),
+				m.getArgAsString(1),
+				m.getArgAsString(2),
+				m.getArgAsFloat(3),
+			};
+			ofLogNotice("ofxCooperHewittOscPen") << "Reading Pen: " << pen.penID << endl;
+		}
+
+		else if ((m.getAddress() == PEN_DONE_OSC_ADDRESS) && (m.getNumArgs() == NUM_PEN_STATUS_ARGS)) {
+			penStatusEvent penStatus = {
+				m.getArgAsString(0),
+				m.getArgAsString(1),
+				m.getArgAsString(2),
+				m.getArgAsInt32(3)
+			};
+			ofLogNotice("ofxCooperHewittOscPen") << "Done reading pen: " << penStatus.penID << endl;
+		}
+
+		else if ((m.getAddress() == PEN_CANCEL_OSC_ADDRESS) && (m.getNumArgs() == NUM_PEN_STATUS_ARGS)) {
+			penStatusEvent penStatus = {
+				m.getArgAsString(0),
+				m.getArgAsString(1),
+				m.getArgAsString(2)
+			};
+			ofLogNotice("ofxCooperHewittOscPen") << "Canceled reading pen: " << penStatus.penID << endl;
+		}
+
+		else if (m.getAddress() == READER_OSC_ADDRESS) {
+			string readerID = m.getArgAsString(0);
+			ofLogNotice("ofxCooperHewittOscPen") << "Reader ID: " << readerID << endl;
+		}
+		
+		else if (m.getAddress() == ALL_READERS_ADDRESS) {
+			int numReaders = m.getArgAsInt32(0);
+			ofLogNotice("ofxCooperHewittOscPen") << "Numbr of readers: " << numReaders << endl;
 		}
 	}
 
--- src/ofxCooperHewittOscPen.h
+++ src/ofxCooperHewittOscPen.h
@@ -10,10 +10,35 @@ public:
 	ofxCooperHewittOscPen();
 	~ofxCooperHewittOscPen();
 
+	void setup(int port);
+
 	string getPenMessages();
 
 protected:
 
+	struct tagEvent {
+		string readerID;
+		string penID;
+		string visitID;
+		string objectType;
+		string tagID;
+		string timeSInceSetup;
+		float progress;
+	};
+
+	struct penEvent {
+		string readerID;
+		string penID;
+		string visitID;
+		float progress;
+	};
+
+	struct penStatusEvent {
+		string readerID;
+		string penID;
+		string visitID;
+		bool isTimeout;
+	};
 	ofxOscReceiver oscReceiver;
 
 };
